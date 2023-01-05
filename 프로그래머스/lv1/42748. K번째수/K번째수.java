6087f8e07c382400f6d231e104fc4845034c3a2f
Jedong <108327853+YouJedong@users.noreply.github.com>
2023-01-06 00:15:26 +0900

    Update K번째수.java

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -3,15 +3,14 @@ import java.util.Arrays;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        int[] temp = {};
 
         for (int i = 0; i < commands.length; i++) {
             int start = commands[i][0];
             int end = commands[i][1];
             int resultNum = commands[i][2];
-
-            temp = new int[(end - start) + 1];
-            temp = Arrays.copyOfRange(array, (start-1) , end);
+            
+            // 특정 인덱스  copyOfRange(배열변수, 시작 인덱스, 끝 인덱스?)
+            int[] temp = Arrays.copyOfRange(array, start-1 , end); 
             Arrays.sort(temp);
             answer[i] = temp[resultNum - 1];
         }
