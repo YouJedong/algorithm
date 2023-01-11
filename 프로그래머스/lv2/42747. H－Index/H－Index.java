be01cd229a4138c43d77e69deb15f6f9c11ff47f
Jedong <108327853+YouJedong@users.noreply.github.com>
2023-01-11 14:17:22 +0900

    Update H－Index.java

--- 프로그래머스/lv2/42747. H－Index/H－Index.java
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -1,4 +1,9 @@
 import java.util.Arrays;
+// 1. H-Index의 최대치인 발표한 논문 편수부터 비교하기 위해 answer변수에 담음
+// 2. 논문 인용 횟수를 기준으로 오름차순으로 정렬한다.
+//    왜? 제일 인용 횟수가 적은 것부터 비교할 때 answer보다 크거나 같다면 뒤에 있는 논문 인용횟수보다 크다는 것이기 때문에 H-Index가 된다.
+// 3. H-Index를 찾는다.
+
 
 class Solution {
     public int solution(int[] citations) {
