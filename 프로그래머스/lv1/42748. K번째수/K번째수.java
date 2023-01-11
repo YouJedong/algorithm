f7c3528389e3618d639b230022637e65de5dd31b
Jedong <108327853+YouJedong@users.noreply.github.com>
2023-01-11 13:27:13 +0900

    Update K번째수.java

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -1,4 +1,9 @@
 import java.util.Arrays;
+// 1. 시작점, 끝점, k번째 수 변수에 담기
+// 2. array를 시작점과 끝점으로 잘라서(copyOfRange) temp배열에 담기
+// 3. 자른 배열을 정렬(sort)
+// 4. k번째수를 answer배열에 담음
+// 5. commands개수만큼 반복
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
@@ -9,8 +14,8 @@ class Solution {
             int end = commands[i][1];
             int resultNum = commands[i][2];
             
-            // 특정 인덱스  copyOfRange(배열변수, 시작 인덱스, 끝 인덱스?)
-            int[] temp = Arrays.copyOfRange(array, start-1 , end); 
+            // 특정 인덱스  copyOfRange(배열변수, 시작 인덱스, 끝 인덱스? 배열 길이?)
+            int[] temp = Arrays.copyOfRange(array, start-1 , end); // ex) 0 ~ 4 -> .copyOfRange(array, 0, 5) 
             Arrays.sort(temp);
             answer[i] = temp[resultNum - 1];
         }
