649c84d9ca393e46af0353409f36a580167ea389
Jedong <108327853+YouJedong@users.noreply.github.com>
2023-01-28 16:22:13 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 128 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -1,21 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    sc.close();
-    long[] result = new long[n + 1];
-    result[0] = 0;
-    if (n > 0) result[1] = 1;
 
-    for (int i = 2; i <= n; i++) {
-      if(n == 0 || n == 1) {
-        break;
-      } else {
-        result[i] = result[i - 1] + result[i - 2];
-      }
-    }
-    System.out.println(result[n]);
+  static long[] fbncRepo;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    br.close();
+    fbncRepo = new long[n + 1];
+
+    System.out.println(fbnc(n));
+
+  }
+  
+  private static long fbnc(int n) {
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+    if(fbncRepo[n] != 0) return fbncRepo[n];
+    return fbncRepo[n] = fbnc(n - 1) + fbnc(n - 2);
   }
 }
