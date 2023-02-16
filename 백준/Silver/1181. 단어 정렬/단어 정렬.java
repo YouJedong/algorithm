703a2f68fe49a418eae6e92c553d4a4df77b6545
Jedong <108327853+YouJedong@users.noreply.github.com>
2023-02-16 22:41:36 +0900

    Update 단어 정렬.java

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -43,3 +43,47 @@ class Main {
     System.out.println(sb);
   }
 }
+// 다른 풀이
+//package test;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.Arrays;
+//import java.util.Comparator;
+//
+//class BaekJoon1181_01 {
+//  public static void main(String[] args) throws IOException {
+//    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//    int N = Integer.parseInt(br.readLine());
+//    String[] arr = new String[N];
+//    for(int i = 0; i < N; i++) {
+//      arr[i] = br.readLine();
+//    }
+//
+//    Arrays.sort(arr, new Comparator<String>() {
+//      @Override
+//      public int compare(String o1, String o2) {
+//        if (o1.length() < o2.length()) {
+//          return -1;
+//        } else if (o1.length() > o2.length()) {
+//          return 1;
+//        } else {
+//          return o1.compareTo(o2);
+//        }
+//      }
+//    });
+//
+//    for (int i = 0; i < arr.length; i++) {
+//      for (int j = i + 1; j < arr.length; j++) {
+//        if (arr[i].length() != arr[j].length() || !arr[i].equals(arr[j])) {
+//          break;
+//        } else {
+//          i++;
+//          continue;
+//        }
+//      }
+//      System.out.println(arr[i]);
+//    }
+//  }
+//}
